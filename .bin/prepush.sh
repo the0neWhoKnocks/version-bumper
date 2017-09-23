8b6b2182eb874a67a95c0b513ac3d305128e57b0
Nox <the0neWhoKnocks@users.noreply.github.com>
2017-09-23 09:42:59 -0700

    Refining script

--- .bin/prepush.sh
+++ .bin/prepush.sh
@@ -18,6 +18,9 @@ MAJOR=$(node -pe "var nums = '$VERSION'.split('.'); nums[0]=+nums[0]+1; nums[1]=
 MINOR=$(node -pe "var nums = '$VERSION'.split('.'); nums[1]=+nums[1]+1; nums[2]=0; nums.join('.')")
 PATCH=$(node -pe "var nums = '$VERSION'.split('.'); nums[2]=+nums[2]+1; nums.join('.')")
 
+# Allows for reading input below during actual git call - assigns stdin to keyboard
+exec < /dev/tty
+
 echo;
 echo " Choose what version to bump to"
 echo;
@@ -27,8 +30,6 @@ echo " (3) Major - $MAJOR"
 echo " (4) Don't bump the version"
 echo;
 
-# capture the user's choice
-echo -n ""
 read selectedOption
 
 case $selectedOption in
@@ -44,11 +45,11 @@ case $selectedOption in
     bump="major"
     newVersion="$MAJOR"
     ;;
-  4)
-    echo "don't bump"
-    ;;
 esac
+# close stdin
+exec <&-
 
+echo;
 if [[ "$bump" != "" ]]; then
   npm version --no-git-tag-version $bump
   handleError $? "Couldn't bump version number."
